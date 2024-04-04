72d09ac6e5f7e3668a48593ef5b6acd24549e53f
Vinicius Fillos <vinifillos@gmail.com>
2024-04-04 07:06:28 -0300

    Bounded wildcards implemented

--- src/application/App.java
+++ src/application/App.java
@@ -5,6 +5,7 @@ import entities.Rectangle;
 import entities.Shape;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class App {
@@ -18,9 +19,20 @@ public class App {
         myCircles.add(new Circle(3.0));
         myShapes.add(new Circle(2.0));
 
-        System.out.println("Total area: " + totalArea(myShapes));
-        System.out.println("Total area: " + totalArea(myCircles));
+//        System.out.println("Total area: " + totalArea(myShapes));
+//        System.out.println("Total area: " + totalArea(myCircles));
 
+        List<Integer> myInts = Arrays.asList(3, 4, 5);
+        List<Double> myDoubles = Arrays.asList(2.1, 3.5, 6.7);
+        List<Object> myObjs = new ArrayList<>();
+
+        myObjs = copy(myInts, myObjs);
+        printList(myObjs);
+
+        myObjs.clear();
+
+        myObjs = copy(myDoubles, myObjs);
+        printList(myObjs);
     }
 
     public static double totalArea(List<? extends Shape> list) {
@@ -29,5 +41,19 @@ public class App {
             sum += s.area();
         }
         return sum;
+    }
+
+    public static List copy(List<? extends Number> source, List<? super Number> destiny) {
+        for(Number number : source) {
+            destiny.add(number);
+        }
+        return destiny;
+    }
+
+    public static void printList(List<?> myObjs) {
+        System.out.println();
+        for(Object obj : myObjs) {
+            System.out.println(obj);
+        }
     }
 }
